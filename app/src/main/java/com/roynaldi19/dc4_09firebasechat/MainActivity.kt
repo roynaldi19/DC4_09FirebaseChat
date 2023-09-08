44a9b3759c3a3a0a3a40c23fcfc515e799902aca
roynaldi19 <roynaldi19@gmail.com>
2023-09-08 08:34:59 +0700

    sign in berhasil

--- app/src/main/java/com/roynaldi19/dc4_09firebasechat/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/dc4_09firebasechat/MainActivity.kt
@@ -1,19 +1,33 @@
 package com.roynaldi19.dc4_09firebasechat
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.roynaldi19.dc4_09firebasechat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+        val firebaseUser = auth.currentUser
+
+        if (firebaseUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+            return
+        }
 
     }
 
@@ -34,6 +48,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun signOut() {
+
+        auth.signOut()
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
 
     }
 }
